e2b37831de0ff3aba0525c05a5427bb7b2b9772a
MinexAutomation <minex.automation@gmail.com>
2019-11-03 19:22:29 -0800

    Added DI container extension to get an intermediate service from a services collection by building an intermediate service provider and requesting the service.

---
+++ source/R5T.Dacia/Code/Extensions/DI/IServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Dacia.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Get a service out of the current state of the service collection.
+        /// </summary>
+        /// <remarks>
+        /// Build a service provider from the current state of the service collection and get a required service.
+        /// </remarks>
+        public static T GetIntermediateRequiredService<T>(this IServiceCollection services)
+        {
+            var intermediateServiceProvider = services.BuildServiceProvider();
+
+            var output = intermediateServiceProvider.GetRequiredService<T>();
+            return output;
+        }
+    }
+}
