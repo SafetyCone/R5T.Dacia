df8ced65a8c3e6fa7997403bcae20be2ae39d9d2
MinexAutomation <minex.automation@gmail.com>
2019-11-05 15:02:47 -0800

    Added service provider extension methods has-service and a try-get-service. Added service helper class.

---
+++ source/R5T.Dacia/Code/Extensions/DI/IServiceProviderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Dacia.Extensions
+{
+    public static class IServiceProviderExtensions
+    {
+        public static bool TryGetService<TService>(this IServiceProvider serviceProvider, out TService service)
+            where TService: class
+        {
+            service = serviceProvider.GetService<TService>();
+
+            var output = ServiceHelper.IsNullService(service);
+            return output;
+        }
+
+        public static bool HasService<TService>(this IServiceProvider serviceProvider)
+            where TService: class
+        {
+            var output = serviceProvider.TryGetService<TService>(out _);
+            return output;
+        }
+    }
+}
