c31dbf9097a01ea9cb9c7f78eb62ef824a255179
MinexAutomation <minex.automation@gmail.com>
2019-11-07 11:13:28 -0800

    Added method to add a singleton by generic type parameter if the service instance input parameter is the null service (null).

--- source/R5T.Dacia/Code/Extensions/DI/IServiceCollectionExtensions.cs
+++ source/R5T.Dacia/Code/Extensions/DI/IServiceCollectionExtensions.cs
@@ -22,6 +22,26 @@ namespace R5T.Dacia.Extensions
             return output;
         }
 
+        /// <summary>
+        /// Adds the <typeparamref name="TImplementation"/> instance as a singleton instance (if not null), else adds the <typeparamref name="TImplementation"/> as a service type implementation.
+        /// </summary>
+        public static IServiceCollection AddSingletonAsTypeIfInstanceNull<TService, TImplementation>(this IServiceCollection services, TImplementation instance)
+            where TService: class
+            where TImplementation: class, TService
+        {
+            var instanceIsNullService = ServiceHelper.IsNullService(instance);
+            if(instanceIsNullService)
+            {
+                services.AddSingleton<TService, TImplementation>();
+            }
+            else
+            {
+                services.AddSingleton<TService>(instance);
+            }
+
+            return services;
+        }
+
         public static IServiceCollection TryAddSingletonFluent<TService, TImplementation>(this IServiceCollection services)
             where TImplementation: class, TService
             where TService: class
