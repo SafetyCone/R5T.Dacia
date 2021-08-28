8a59d7600e06895f055f2e3f57f7ae734ec063fd
David Coats <david@getsafetycone.com>
2021-08-28 15:45:00 -0700

    Created R5T.Dacia.T002 library and moved all service-related market attributes over from the R5T.Dacia.Extensions repository.

---
+++ source/R5T.Dacia.T002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Dacia.T002
+{
+    /// <summary>
+    /// Service-related attribute types. Attributes for service definitions, implementations, and aggregations.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
